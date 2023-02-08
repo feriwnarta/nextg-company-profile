2cc325ff5d3b04f39ebab9b43173f5ca0b7e16e4
feriwnarta <feriwnarta26@gmail.com>
2023-02-08 16:59:36 +0700

    update baru

--- index.php
+++ index.php
@@ -11,6 +11,10 @@
   <link rel="stylesheet" href="css/style.css">
   <link rel="stylesheet" href="css/component-button.css">
   <link rel="stylesheet" href="css/component-font.css">
+  <link rel="stylesheet" href="css/component-spacer.css">
+  <link rel="stylesheet" href="css/component-color.css">
+  <link rel="stylesheet" href="css/component-icon.css">
+  <link rel="stylesheet" href="assets/css/satoshi.css">
 
   <title>Hello, world!</title>
 </head>
@@ -19,12 +23,12 @@
   <!-- Start Navbar -->
   <nav class="navbar bg-none navbar-expand-lg navbar-light">
     <div class="container d-flex">
-      <img src="assets/Logo.png" alt="logo nextg" width="120" height="auto">
+      <img src="assets/img/Logo.png" alt="logo nextg" width="120" height="auto">
 
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav ms-auto me-auto">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link active subtitle-3-regular" aria-current="page" href="#">Home</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">Service</a>
@@ -38,7 +42,7 @@
         </ul>
 
         <!-- contact us -->
-        <button type="button" class="btn-primary-text">Contact us</button>
+        <button type="button" class="btn-primary-text-only">Contact us</button>
 
 
       </div>
@@ -54,25 +58,303 @@
   <!-- End Of Navbar -->
 
   <!-- Hero Section -->
-  <div class="container">
+  <div class="container margin-top-124">
     <div class="row">
       <div class="col-12 col-sm-5">
-        <div class="title-2-medium">
+        <div class="title-2-bold">
           One Stop
-          IT Solutions
-          For Startup,
-          Small Business,
-          and Corporate.
+          <br>
+          <span class="primary-blue-pigment-90">IT Solutions</span>
+          <br>
+          <span class="primary-blue-pigment-main">For</span> <span class="secondary-medium-red-violet-main">Startup,</span>
+          <span class="secondary-old-gold-main">Small Business,</span>
+          <span class="secondary-medium-sea-grean">and Corporate.</span>
         </div>
+
+        <div class="primary-blue-pigment-90 subtile-2-regular margin-top-16">
+          When business and technology are synergized.
+        </div>
+
+        <button class="btn btn-icon-text margin-top-40"><i class="arrow-right"></i>See what we can do</button>
+
       </div>
 
       <div class="col-12 col-sm-7">
-        <img class="img-fluid" src="assets/device-hero.png" alt="gambar device handphone dan table" width="939" height="auto">
+        <img class="img-fluid" src="assets/img/device-hero.png" alt="gambar device handphone dan table" width="939" height="auto">
       </div>
     </div>
   </div>
-
   <!-- End Of Hero Section -->
+
+  <!-- Client Section -->
+  <div class="container margin-top-156">
+    <div class="row align-items-center justify-content-between">
+      <div class="col-12 col-sm-1 offset-sm-2">
+        <img src="assets/img/ANTink Logo.png" alt="logo antink" class="img-fluid">
+      </div>
+      <div class="col-12 col-sm-1">
+        <img src="assets/img/tru.png" alt="logo true" class="img-fluid">
+      </div>
+      <div class="col-12 col-sm-1">
+        <img src="assets/img/simut.png" alt="logo simut" class="img-fluid">
+      </div>
+      <div class="col-12 col-sm-1">
+        <img src="assets/img/pik.png" alt="logo bgm pik" class="img-fluid">
+      </div>
+      <div class="col-12 col-sm-1">
+        <img src="assets/img/cahaya-senja.png" alt="logo cahaya senja" class="img-fluid">
+      </div>
+      <div class="col-12 col-sm-1">
+        <img src="assets/img/charlie.png" alt="logo simut" class="img-fluid">
+      </div>
+      <div class="col-sm-2"></div>
+    </div>
+  </div>
+  <!-- End Client Section -->
+
+  <!-- Section Our Services -->
+  <section id="our-services" class="margin-top-156">
+    <!-- our services title -->
+    <div class="container text-center">
+      <div class="heading-1-bold primary-blue-pigment-main">Our Services</div>
+      <div class="subtitle-2-regular margin-top-24 color-neutral-80">Helping business at different stages with their unique needs</div>
+    </div>
+    <!-- end our services title -->
+
+    <!-- content our services -->
+    <div class="container margin-top-80">
+      <div class="row">
+        <div class="col-sm-6 offset-sm-1">
+          <div class="heading-3-bold primary-blue-pigment-main">Website Development Services</div>
+          <div class="subtitle-3-regular color-neutral-80 margin-top-16">Comprehensive services ranging from design, redesign, programming and technical support for website creation with high standards with the aim of increasing conversion on the website.</div>
+        </div>
+        <div class="col-sm-4 offset-sm-1 d-flex justify-content-sm-between align-items-center">
+          <a href="#" class="subtitle-2-bold">
+            Website Development Service
+          </a>
+          <i class="arrow-outward align-items"></i>
+        </div>
+      </div>
+
+      <div class="row margin-top-68">
+        <div class="col-sm-6 offset-sm-1">
+          <div class="heading-3-bold primary-blue-pigment-main">Custom Application Services</div>
+          <div class="subtitle-3-regular color-neutral-80 margin-top-16">We develop applications with the latest implementation methods for all kinds of platforms iOS, Android, Web Apps.</div>
+        </div>
+        <div class="row col-sm-4 offset-sm-1">
+          <div class="d-flex justify-content-sm-between align-items-center">
+            <a href="#" class="subtitle-2-bold">
+              UI/UX Design Service
+            </a>
+            <i class="arrow-outward align-items"></i>
+          </div>
+          <div class="d-flex justify-content-sm-between align-items-center">
+            <a href="#" class="subtitle-2-bold">
+              Mobile App Development
+            </a>
+            <i class="arrow-outward align-items"></i>
+          </div>
+          <div class="d-flex justify-content-sm-between align-items-center">
+            <a href="#" class="subtitle-2-bold">
+              Web App Development
+            </a>
+            <i class="arrow-outward align-items"></i>
+          </div>
+        </div>
+      </div>
+
+      <div class="row margin-top-68">
+        <div class="col-sm-6 offset-sm-1">
+          <div class="heading-3-bold primary-blue-pigment-main">Enterprise Resource Planning Systems</div>
+          <div class="subtitle-3-regular color-neutral-80 margin-top-16">Development and Implementation of ERP system, Configuration, Data Migration, Functional and Technical Consultant, Server Deployment, Project Manager, training and support.</div>
+        </div>
+        <div class="row col-sm-4 offset-sm-1">
+          <div class="d-flex justify-content-sm-between align-items-center">
+            <a href="#" class="subtitle-2-bold">
+              ERP On Premise
+            </a>
+            <i class="arrow-outward align-items"></i>
+          </div>
+          <div class="d-flex justify-content-sm-between align-items-center">
+            <a href="#" class="subtitle-2-bold">
+              Open Source ERP
+            </a>
+            <i class="arrow-outward align-items"></i>
+          </div>
+          <div class="d-flex justify-content-sm-between align-items-center">
+            <a href="#" class="subtitle-2-bold">
+              Cloud-based ERP
+            </a>
+            <i class="arrow-outward align-items"></i>
+          </div>
+          <div class="d-flex justify-content-sm-between align-items-center">
+            <a href="#" class="subtitle-2-bold">
+              Hybrid ERP
+            </a>
+            <i class="arrow-outward align-items"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- end content our services -->
+  </section>
+  <!-- End Section Our Services -->
+
+
+  <!-- Section Erp Solution -->
+  <section id="epr-solution" class="margin-top-156">
+    <div class="container text-center">
+      <div class="primary-blue-pigment-main heading-1-bold">ERP Solutions</div>
+      <div class="subtitle-2-regular color-neutral-80">Supporting business through digital ecosystem solutions</div>
+    </div>
+
+    <div class="container margin-top-80">
+      <div class="row">
+        <div class="col-sm-3 offset-sm-1 d-sm-flex flex-column justify-content-between">
+          <div>
+            <img src="assets/img/kepala-roda.png" width="48px" alt="icon manufacturing">
+            <div class="margin-top-16 subtitle-1-bold">Manufacturing Company</div>
+            <ul class="margin-top-16 body-text-1-regular color-neutral-90">
+              <li>Purchasing</li>
+              <li>Inventory</li>
+              <li>Sales</li>
+              <li>Accounting and Finance</li>
+              <li>Manufacturing</li>
+            </ul>
+          </div>
+
+          <div class="">
+            <a href="#" class="subtitle-2-bold">
+              Know more
+            </a>
+            <i class="arrow-right-blue-main align-items"></i>
+          </div>
+        </div>
+
+        <div class="col-sm-3 offset-sm-1 d-sm-flex flex-column justify-content-between">
+          <div>
+            <img src="assets/img/distribution-company.png" width="48px" alt="icon distribution">
+            <div class="margin-top-16 subtitle-1-bold">Distribution Company</div>
+            <ul class="margin-top-16 body-text-1-regular color-neutral-90">
+              <li>Purchasing</li>
+              <li>Inventory</li>
+              <li>Sales</li>
+              <li>Accounting and Finance</li>
+            </ul>
+          </div>
+
+          <div class="button">
+            <div class="">
+              <a href="#" class="subtitle-2-bold">
+                Know more
+              </a>
+              <i class="arrow-right-blue-main align-items"></i>
+            </div>
+          </div>
+
+        </div>
+
+
+
+        <div class="col-sm-3 offset-sm-1 d-sm-flex flex-column justify-content-beetwen">
+          <div>
+            <img src="assets/img/kepala-roda.png" width="48px" alt="icon manufacturing">
+            <div class="margin-top-16 subtitle-1-bold">FnB Company</div>
+            <ul class="margin-top-16 body-text-1-regular color-neutral-90">
+              <li>Purchasing</li>
+              <li>Inventory</li>
+              <li>Sales</li>
+              <li>Accounting and Finance</li>
+              <li>Manufacturing</li>
+              <li>Point Of Sales</li>
+            </ul>
+          </div>
+          <div>
+            <div class="">
+              <a href="#" class="subtitle-2-bold">
+                Know more
+              </a>
+              <i class="arrow-right-blue-main align-items"></i>
+            </div>
+          </div>
+        </div>
+
+
+
+        <div class="col-1 col-sm-3"></div>
+        <div class="col-1 col-sm-3"></div>
+      </div>
+    </div>
+  </section>
+  <!-- End Section Erp Solution -->
+
+
+  <!-- Section some of our recent work -->
+  <section id="some-of-our-recent-work" class="margin-top-156">
+    <div class="container">
+      <div class="row">
+        <div class="col-5 offset-1">
+          <div class="row d-flex flex-column">
+            <div class="col">
+              <img src="assets/img/sinar-mutiara-web-erp.png" class="img-fluid" alt="sinar mutiara web erp">
+            </div>
+            <div class="col">
+              <img src="assets/img/sinar-mutiara-web-erp.png" class="img-fluid" alt="sinar mutiara web erp">
+            </div>
+          </div>
+        </div>
+
+        <div class="col-5">
+          <div class="row d-flex flex-column">
+            <div class="col">
+              <img src="assets/img/tru-color.png" class="img-fluid" alt="sinar mutiara web erp">
+              <div class="subtitle-1-bold margin-top-32">TRUcolor Web E-Commerce</div>
+              <div class="subtitle-3-regular color-neutral-90 margin-top-16">24 hours of processing orders, receiving payments, managing shipping and logistics, and providing customer support services.</div>
+            </div>
+
+            <div class="col margin-top-40">
+              <img src="assets/img/bgm-pik.png" class="img-fluid" alt="sinar mutiara web erp">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+
+    <!-- <div class="container text-center">
+      <div class="primary-blue-pigment-main heading-1-bold">Some of our Recent Work</div>
+      <div class="subtitle-2-regular color-neutral-80">The work that we have done from several types of companies and types of <br> projects</div>
+    </div>
+
+    <div class="row">
+      <div class="col-5 offset-1">
+        <img src="assets/img/sinar-mutiara-web-erp.png" class="img-fluid" alt="sinar mutiara web erp">
+      </div>
+      <div class="col-5 bg-danger flex-column d-flex align-self-start">
+        <img src="assets/img/tru-color.png" class="img-fluid" alt="sinar mutiara web erp">
+        <div>
+          TRUcolor Web E-Commerce
+        </div>
+        <div>24 hours of processing orders, receiving payments, managing shipping and logistics, and providing customer support services.</div>
+      </div>
+
+      <div class="col-5 offset-1">
+        <img src="assets/img/sinar-mutiara-web-erp.png" class="img-fluid" alt="sinar mutiara web erp">
+      </div>
+
+      <div class="col-5 d-flex flex-column align-self-start">
+        <img src="assets/img/sinar-mutiara-web-erp.png" class="img-fluid" alt="sinar mutiara web erp">
+        <div>BGM PIK Mobile Apps ( ios & android )</div>
+      </div>
+
+    </div> -->
+
+
+  </section>
+  <!-- End section some of our recet work -->
+
+
 
 
 
