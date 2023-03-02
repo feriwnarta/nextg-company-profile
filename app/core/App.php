9472d5b15b491d556f3a3558139a530ad9872230
feriwnarta <feriwnarta26@gmail.com>
2023-03-02 16:58:27 +0700

    updarte

--- app/core/App.php
+++ app/core/App.php
@@ -19,13 +19,14 @@ class App
         $url = $this->clearUrl();
 
         // ambil controller berdasarkan data url yang sudah diparse ke array menggunakan function clear url
-        if (isset($url[0]) && file_exists('app/controllers/' . $url[0] . '.php')) {
+        if (isset($url[0]) && $this->file_exist_case_insensitive('app/controllers/' . $url[0] . '.php')) {
             $this->controller = $url[0];
             unset($url[0]);
         }
 
-        require_once('app/controllers/' . $this->controller . '.php');
-        // buat objek controller 
+        // require_once('app/controllers/' . $this->controller . '.php');
+        $this->require_once_case_insensitive('app/controllers/' . $this->controller . '.php');
+        // buat objek controller
         $this->controller = new $this->controller;
 
         // ambil method berdasarkan data url yang sudah diparse ke array menggunakan function clear url
@@ -61,4 +62,45 @@ class App
             return array();
         }
     }
+
+
+    function file_exist_case_insensitive($file_path) {
+        // ambil nama file dan direktori
+        $dirname = dirname($file_path);
+        $filename = basename($file_path);
+
+        // cari semua file dalam direktori
+        $files = glob($dirname . '/*');
+
+        // loop setiap file dan periksa apakah ada file dengan nama yang sama tanpa memperhatikan case sensitive
+        foreach ($files as $file) {
+            if (strcasecmp(basename($file), $filename) == 0) {
+                return true;
+            }
+        }
+
+        // jika tidak ditemukan, kembalikan false
+        return false;
+    }
+
+    function require_once_case_insensitive($file_path) {
+        // ambil nama file dan direktori
+        $dirname = dirname($file_path);
+        $filename = basename($file_path);
+
+        // cari semua file dalam direktori
+        $files = glob($dirname . '/*');
+
+        // loop setiap file dan periksa apakah ada file dengan nama yang sama tanpa memperhatikan case sensitive
+        foreach ($files as $file) {
+            if (strcasecmp(basename($file), $filename) == 0) {
+                require_once($file);
+                return true;
+            }
+        }
+
+        // jika tidak ditemukan, tampilkan pesan kesalahan
+        return false;
+    }
+
 }
